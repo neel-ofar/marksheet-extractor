82977cd8b4b916174a3ceabd62795050487f3897
neelofar shaik <skneeluneelofar@gmail.com>
2026-01-26 02:23:32 +0530

    backend commits

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 import os
 from groq import Groq
 
-from apps.utils import image_to_base64, get_llm_result
+from app.utils import image_to_base64, get_llm_result
 from models import Extraction
 
 load_dotenv()
